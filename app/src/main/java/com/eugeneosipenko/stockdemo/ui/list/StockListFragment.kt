60e2d503f5b815b2783bfc958812c77a33cd5a2f
Eugene Osipenko <jonez364@gmail.com>
2021-08-14 12:52:57 +0300

    initial commit

---
+++ app/src/main/java/com/eugeneosipenko/stockdemo/ui/list/StockListFragment.kt
@@ -0,0 +1,44 @@
+package com.eugeneosipenko.stockdemo.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.eugeneosipenko.stockdemo.databinding.FragmentDashboardBinding
+
+class StockListFragment : Fragment() {
+
+    private lateinit var stockListViewModel: StockListViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        stockListViewModel =
+            ViewModelProvider(this).get(StockListViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        stockListViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
